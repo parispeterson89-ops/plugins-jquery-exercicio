1d67ff50e14062ee9971133e2780c5cb960e1412
Peterson Paris <140709138+parispeterson89-ops@users.noreply.github.com>
2025-03-22 03:51:37 -0300

    fiz uma modificação menor

--- js/main.js
+++ js/main.js
@@ -1,42 +1,47 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
-
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
-
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+$('#telefone').mask('(00) 00000-0000', { placeholder: '(DDD) 12345-6789' });
+$('#cpf').mask('000.000.000-00', { placeholder: '123.456.789-00' });
+$('#cep').mask('00000-000', { placeholder: '01234-567' });
 
 $('form').validate({
-    rules: {
-        nome: {
-            requerido: true
-        },
-        email: {
-            requerido: true,
-            email: true
-        },
-        telefone: {
-            requerido: true
-        },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
-    },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
-    },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
-})
+	rules: {
+		nome: {
+			required: true,
+		},
+		email: {
+			required: true,
+			email: true,
+		},
+		telefone: {
+			required: true,
+		},
+		endereco: {
+			required: true,
+		},
+		cep: {
+			required: true,
+		},
+		cpf: {
+			required: true,
+		},
+	},
+	messages: {
+		nome: 'Por favor, insira seu nome completo.',
+		email: {
+			required: 'Por favor, insira seu e-mail.',
+			email: 'Por favor, insira um e-mail válido.',
+		},
+		telefone: 'Por favor, insira seu telefone.',
+		endereco: 'Por favor, insira seu endereço.',
+		cep: 'Por favor, insira seu CEP.',
+		cpf: 'Por favor, insira seu CPF.',
+	},
+	submitHandler: function (form) {
+		alert('Sua requisição foi enviada para análise, parabéns pela aquisição!');
+		form.reset();
+	},
+	invalidHandler: function (form, validator) {
+		alert(
+			'Por favor, preencha os campos corretamente para prosseguir com a compra!'
+		);
+	},
+});
